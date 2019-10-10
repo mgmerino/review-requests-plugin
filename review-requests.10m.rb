229fd38e675155c9e3a56aa914a4466a8529b592
Manuel González <manuel.gonzalez@jobandtalent.com>
2019-10-10 20:20:16 +0200

    update icons

--- review-requests.10m.rb
+++ review-requests.10m.rb
@@ -23,7 +23,7 @@ github_org = config["github_org"]
 blacklisted_prs = config["blacklisted_prs"]
 
 graphql_query = "{\r\n  search(query: \"org:#{github_org} is:pr is:open review-requested:#{github_handler}\", type: ISSUE, last: 10) {\r\n    issueCount\r\n    edges {\r\n      node {\r\n        ... on PullRequest {\r\n          repository {\r\n            name\r\n          }\r\n          author {\r\n            login\r\n          }\r\n          reviews{\r\n            totalCount\r\n          }\r\n          reviewRequests{\r\n            totalCount\r\n          }\r\n          comments{\r\n            totalCount\r\n          }\r\n          createdAt\r\n          number\r\n          url\r\n          title\r\n        }\r\n      }\r\n    }\r\n  }\r\n}"
-ICONS = ["🥬", "🔆", "✴️", "🚩"]
+ICONS = ["🥬", "🍋", "🍊", "🌶"]
 
 client = GraphqlClient.new(token: config["github_token"])
 data = client.do_query(graphql_query)
